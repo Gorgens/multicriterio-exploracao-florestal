11690ef632282690609a552cf80dfc57c73cb810
Eric Bastos Gorgens <2597395+Gorgens@users.noreply.github.com>
2021-04-10 10:50:14 -0300

    Optimize gap extraction

--- gapDynamics/04-03gaps.R
+++ gapDynamics/04-03gaps.R
@@ -2,21 +2,35 @@ require(raster)
 require(ForestGapR)
 require(tools)
 require(dplyr)
+require(sf)
+require(purrr)
+require(rgeos)
 
 # Extract gaps
-chmList = list.files('./ducke_2017/chm/', pattern = '\\.tif$')
+chmList = chmList[39:56]
 
-threshold = 10
-size = c(9, 100000)
-
-for(i in chmList){
-  gapsRaster = raster(paste0('./ducke_2017/chm/', i))
+shapefileGaps = function(file,
+                          inPath = './tanguro_2018/chm/',
+                          outPath = './tanguro_2018/gaps/',
+                          threshold = 10,
+                          size = c(9, 100000)){
+  gapsRaster = raster(paste0(inPath, file))
   gaps = getForestGaps(chm_layer=gapsRaster, 
                        threshold=threshold, size=size)
   gapsShape = GapSPDF(gap_layer=gaps)
-  gapsStats = GapStats(gap_layer=gaps, chm_layer=gapsRaster)
-  gapsStats = as.data.frame(as.matrix(gapsStats))
-  gapsShape<-merge(gapsShape,gapsStats, by="gap_id")
-  shapefile(gapsShape, filename=paste0('./ducke_2017/gaps/', 
-                                       tools::file_path_sans_ext(i),'.shp'))
+  shapefile(gapsShape, filename=paste0(outPath, tools::file_path_sans_ext(file),'.shp'))
+  return(paste0('Ready: ', file))
 }
+
+lapply(chmList, shapefileGaps)
+
+# Merge gaps
+list.files(inPath, pattern = '\\.shp$', full.names = TRUE) %>% 
+  map(read_sf) %>%
+  do.call(rbind, .) %>%
+  write_sf('./tanguro_2018/tanguro_2018gaps.shp')
+shapefile('./tanguro_2018/tanguro_2018gaps.shp') %>%
+  gBuffer(tanguro_2018gaps, byid=TRUE, width=0) %>%
+  aggregate(tanguro_2018gaps) %>%
+  disaggregate(tanguro_2018gaps) %>%
+  shapefile('./tanguro_2018/tanguro_2018gapsAggregated.shp', overwrite=TRUE)
