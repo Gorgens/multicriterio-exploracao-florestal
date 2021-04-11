af929d51dc949ebaeb9047deb0f6cf5157f1b8bd
Eric Bastos Gorgens <2597395+Gorgens@users.noreply.github.com>
2021-04-11 16:58:52 -0300

    plot gaps frequency histogram

--- gapDynamics/04-03gaps.R
+++ gapDynamics/04-03gaps.R
@@ -7,11 +7,12 @@ require(purrr)
 require(rgeos)
 
 # Extract gaps
-chmList = list.files('./tanguro_2018/chm/', pattern = '\\.tif$')
+chmList = list.files('./jari_2020/a/chm/', pattern = '\\.tif$')
+# chmList = chmList[44:51]
 
 shapefileGaps = function(file,
-                          inPath = './tanguro_2018/chm/',
-                          outPath = './tanguro_2018/gaps/',
+                          inPath = './jari_2020/a/chm/',
+                          outPath = './jari_2020/a/gaps/',
                           threshold = 10,
                           size = c(9, 100000)){
   gapsRaster = raster(paste0(inPath, file))
@@ -25,12 +26,12 @@ shapefileGaps = function(file,
 lapply(chmList, shapefileGaps)
 
 # Merge gaps
-list.files(inPath, pattern = '\\.shp$', full.names = TRUE) %>% 
+list.files('./jari_2020/a/gaps/', pattern = '\\.shp$', full.names = TRUE) %>% 
   map(read_sf) %>%
   do.call(rbind, .) %>%
-  write_sf('./tanguro_2018/tanguro_2018gaps.shp')
-shapefile('./tanguro_2018/tanguro_2018gaps.shp') %>%
-  gBuffer(tanguro_2018gaps, byid=TRUE, width=0) %>%
-  aggregate(tanguro_2018gaps) %>%
-  disaggregate(tanguro_2018gaps) %>%
-  shapefile('./tanguro_2018/tanguro_2018gapsAggregated.shp', overwrite=TRUE)
+  write_sf('./jari_2020/a/jari_2020gaps.shp')
+shapefile('./jari_2020/a/jari_2020gaps.shp') %>%
+  gBuffer(byid=TRUE, width=0) %>%
+  aggregate() %>%
+  disaggregate() %>%
+  shapefile('./jari_2020/a/jari_2020gapsAggregated.shp', overwrite=TRUE)
--- gapDynamics/04-04gapdistr.R
+++ gapDynamics/04-04gapdistr.R
@@ -1,10 +1,49 @@
 require(raster)
-library(plotly)
+require(sp)
+library(ggplot2)
 
 # Distribution gaps
-gaps.shp = shapefile('./zf2a_gaps/zf2a_gaps.shp')
-fig = plot_ly(x = ~gaps.shp$area, 
-               type = "histogram",
-               histnorm = "probability")
-fig = layout(fig, bargap=0.1)
-fig
+gapsDucke2017 = shapefile('./ducke_2017/ducke_2017gapsAggregated.shp')
+crs(gapsDucke2017) = '+proj=utm +zone=21 +south +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'
+gapsDucke2017$area = area(gapsDucke2017) / 10000
+# max(gapsDucke2017$area)
+ggplot(gapsDucke2017@data, aes(area)) +
+  geom_histogram(aes(y = stat(count / sum(count)))) +
+  xlim(0, 0.05) + ylim(0, 0.5) + ggtitle('Ducke 2017')
+
+gapsDucke2020 = shapefile('./ducke_2020/ducke_2020gapsAggregated.shp')
+gapsDucke2020$area = area(gapsDucke2020) / 10000
+# max(gapsDucke2020$area)
+ggplot(gapsDucke2020@data, aes(area)) +
+  geom_histogram(aes(y = stat(count / sum(count)))) + 
+  xlim(0, 0.05) + ylim(0, 0.5) + ggtitle('Ducke 2020')
+
+gapsTanguro2018 = shapefile('./tanguro_2018/tanguro_2018gapsAggregated.shp')
+crs(gapsTanguro2018) = '+proj=utm +zone=22 +south +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'
+gapsTanguro2018$area = area(gapsTanguro2018) / 10000
+# max(gapsTanguro2018$area)
+ggplot(gapsTanguro2018@data, aes(area)) +
+  geom_histogram(aes(y = stat(count / sum(count)))) + 
+  xlim(0, 0.05) + ylim(0, 0.5) + ggtitle('Tanguro 2018')
+
+gapsTanguro2020 = shapefile('./tanguro_2020/tanguro_2020gapsAggregated.shp')
+gapsTanguro2020$area = area(gapsTanguro2020) / 10000
+# max(gapsTanguro2020$area)
+ggplot(gapsTanguro2020@data, aes(area)) +
+  geom_histogram(aes(y = stat(count / sum(count)))) +
+  xlim(0, 0.05) + ylim(0, 0.5) + ggtitle('Tanguro 2020')
+
+gapsTapajos2017 = shapefile('./tapajos_2017/tapajos_2017gapsAggregated.shp')
+crs(gapsTapajos2017) = '+proj=utm +zone=21 +south +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs'
+gapsTapajos2017$area = area(gapsTapajos2017) / 10000
+# max(gapsTapajos2017$area)
+ggplot(gapsTapajos2017@data, aes(area)) +
+  geom_histogram(aes(y = stat(count / sum(count)))) + 
+  xlim(0, 0.05) + ylim(0, 0.5) + ggtitle('Tapajos 2017')
+
+gapsTapajos2020 = shapefile('./tapajos_2020/tapajos_2020gapsAggregated.shp')
+gapsTapajos2020$area = area(gapsTapajos2020) / 10000
+# max(gapsTapajos2020$area)
+ggplot(gapsTapajos2020@data, aes(area)) +
+  geom_histogram(aes(y = stat(count / sum(count)))) + 
+  xlim(0, 0.05) + ylim(0, 0.5) + ggtitle('Tapajos 2020')
